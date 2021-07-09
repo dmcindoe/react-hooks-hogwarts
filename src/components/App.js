fbeae15eaee0d9f61b8511c4165e48faff073b65
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-09 14:23:21 -0400

    Completed deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Filter from "./Filter"
 import TileContainer from "./TileContainer"
-
 import hogs from "../porkers_data";
 
 function App() {
+  const [hogArray, setHogArray] = useState(hogs)
+  const [isChecked, setIsChecked] = useState(false)
+
+  const toggleGreased = hogArray.filter(hog => {
+    if (!isChecked) {
+      return true
+    } else { 
+      return (isChecked === hog.greased)
+    } 
+  })
+
+  const handleCheckBox = () => {
+    setIsChecked(isChecked => !isChecked)
+  }
+
   return (
     <div className="App">
       <Nav />
-      <Filter />
-      <TileContainer hogs={hogs} />
+      <Filter 
+        onCheckClick={handleCheckBox}
+        setHogArray={setHogArray}
+        hogs={hogArray}
+      />
+      <TileContainer hogs={toggleGreased} />
     </div>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,35 @@
 import React from "react";
 
-const Filter = () => {
-    return (
+const Filter = ({ onCheckClick, hogs, setHogArray }) => {
+  const sortByName = [...hogs].sort((a,b) => {
+    const nameA = a.name.toUpperCase()
+    const nameB = b.name.toUpperCase()
+    if(nameA < nameB){
+      return -1;
+      }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  })
+  const sortByWeight = [...hogs].sort((a, b) => a.weight - b.weight )
+
+  const handleSortByName = () => {
+    setHogArray(sortByName)
+  }
+  const handleSortByWeight = () => {
+    setHogArray(sortByWeight)
+  }
+
+  return (
     <div className="FilterContainer">
-        <label>Greased </label>
-        <input type="checkbox" />
-        <button>Name</button>
-        <button>Weight</button>
+      <label>Greased</label>
+      <input type="checkbox" onClick={onCheckClick} />
+      <button onClick={handleSortByName}>Name</button>
+      <button onClick={handleSortByWeight}>Weight</button>
     </div>
-    )
+  )
 }
 
 export default Filter
+  
