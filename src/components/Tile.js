89bca4547d8c875fb9ae808eccb1a30d6e740c1f
Andrew McIndoe <amcindoe@powerhrg.com>
2021-07-09 09:57:44 -0400

    Friday Pair, push 1

---
+++ src/components/Tile.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Tile = ({hog}) => {
+    console.log(hog)
+    return (
+    <div className= "ui eight wide column">
+        <img/>
+        <h2>name</h2>
+
+    </div>
+ ) 
+}
+
+
+export default Tile
---
+++ src/components/TileContainer.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Tile from "./Tile.js"
+
+const TileContainer = ({hogs}) => {
+    return (
+        <div className="ui grid container">
+           { hogs.map((hog) => 
+            <Tile hog={hog}  key={hog.name} />
+          
+            
+           
+            )}
+
+
+
+        </div>
+    )
+}
+
+export default TileContainer
